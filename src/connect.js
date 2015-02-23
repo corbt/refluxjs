bb8763d6c4e952ec5498e38ed5aa9fbec126478c
Kyle Corbitt <kylecorbitt@gmail.com>
2015-02-23 10:34:02 +0000

    experimental lazy eval of connect mixin

--- src/connect.js
+++ src/connect.js
@@ -4,6 +4,10 @@ var Reflux = require('./index'),
 module.exports = function(listenable,key){
     return {
         getInitialState: function(){
+            if (typeof listenable === "string") {
+                listenable = eval(listenable);
+            }
+            
             if (!_.isFunction(listenable.getInitialState)) {
                 return {};
             } else if (key === undefined) {
@@ -13,6 +17,10 @@ module.exports = function(listenable,key){
             }
         },
         componentDidMount: function(){
+            if (typeof listenable === "string") {
+                listenable = eval(listenable);
+            }
+
             _.extend(this,Reflux.ListenerMethods);
             var me = this, cb = (key === undefined ? this.setState : function(v){me.setState(_.object([key],[v]));});
             this.listenTo(listenable,cb);
